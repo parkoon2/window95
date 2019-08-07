10e26b6dc80f20f325f8fc902b78eab9dd68da07
unknown <shin.minsoo@GNRHQ10-NE101EB.LGE.NET>
2019-08-07 17:56:18 +0900

    feat. window icon 작업중

--- pages/index.js
+++ pages/index.js
@@ -3,162 +3,49 @@ import WindowIcon from '../components/WindowIcon'
 import Icon from '../components/Icon'
 // import '../styles/home.css'
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="window">
-                <ul className="window__container">
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                    <li>
-                        <Icon
-                            className="item"
-                            name="computer"
-                            title={'My Computer'}
-                            styles={{ margin: '10px' }}
-                        />
-                    </li>
-                </ul>
-            </div>
-        )
+const INITIAL_ICONS = [
+    {
+        name: 'computer',
+        title: 'My Computer',
+        pos: {
+            x: 0,
+            y: 0
+        }
+    },
+    {
+        name: 'recyclebin',
+        title: 'Recycle Bin',
+        pos: {
+            x: 0,
+            y: 100
+        }
     }
+]
+
+const Home = () => {
+    const renderIcons = () =>
+        INITIAL_ICONS.map((icon, index) => (
+            <li
+                key={index}
+                className="item"
+                style={{ top: icon.pos.y, left: icon.pos.x }}
+            >
+                <Icon name={icon.name} title={icon.title} />
+            </li>
+        ))
+    return (
+        <div className="window">
+            <ul className="window__container">
+                {renderIcons()}
+                {/* <li className="item" style={{ top: '0', left: '0' }}>
+                    <Icon name="computer" title={'My Computer'} />
+                </li>
+                <li className="item" style={{ top: '100px', left: '0' }}>
+                    <Icon name="recyclebin" title={'My Computer'} />
+                </li> */}
+            </ul>
+        </div>
+    )
 }
 
 export default Home
