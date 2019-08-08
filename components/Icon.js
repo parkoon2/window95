c8cca21b4322ddc085ce9ede122149b0741c22f1
parkoon <par_koon@naver.com>
2019-08-08 12:10:52 +0900

    feat. 다른 아이콘을 클릭 했을 때 내 아이콘 포커스 제거

--- components/Icon.js
+++ components/Icon.js
@@ -1,13 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useRef, useEffect } from 'react'
 import classnames from 'classnames'
 import '../styles/icon.css'
 import { withIconContext, iconContext } from '../context/iconContext'
 
-function Icon({ name, size, title, styles, className = '', pressed }) {
-    const icon = useContext(iconContext)
+function Icon({ name, size, title, styles, className = '' }) {
+    // const icon = useContext(iconContext)
+
+    const [pressed, setPress] = useState(false)
+    const containerRef = useRef(null)
+
+    const handleClickOutside = e => {
+        console.log('containerRef,', containerRef)
+        if (containerRef.current && !containerRef.current.contains(e.target)) {
+            setPress(false)
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [])
 
     return (
-        <div className={`icon__container ${className}`} style={{ ...styles }}>
+        <div
+            className={`icon__container ${className}`}
+            style={{ ...styles }}
+            onClick={() => setPress(!pressed)}
+            ref={containerRef}
+        >
             <img
                 className={classnames('icon', size)}
                 src={`static/icons/${name}.ico`}
@@ -25,4 +47,4 @@ function Icon({ name, size, title, styles, className = '', pressed }) {
     )
 }
 
-export default withIconContext(Icon)
+export default Icon
