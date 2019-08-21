64b8c2033462c0e812eb8bf26ca865fe3fcc7d59
parkoon <par_koon@naver.com>
2019-08-21 17:06:39 +0900

    internet 작업중

--- components/internet/Google.js
+++ components/internet/Google.js
@@ -1,11 +1,60 @@
 import Layout from '../window/Layout'
+import Button from '../Button'
+import axios from 'axios'
+import { useState, useRef } from 'react'
 
 const Google = ({ x, y, onClose }) => {
+    const [searchValue, setSearchValue] = useState('')
+
+    const nomalSearchRef = useRef()
+    const directSearchRef = useRef()
+    const formRef = useRef()
+
+    const handleOnChange = e => {
+        setSearchValue(e.target.value)
+    }
+
     return (
-        <Layout x={x} y={y} onClose={onClose} title="Internet">
+        <Layout
+            x={x}
+            y={y}
+            onClose={onClose}
+            title="Internet"
+            noActionbar={true}
+            width="670px"
+            height="480px"
+        >
             {/* <iframe src="https://www.youtube.com/" /> */}
-            <form action="http://google.com/search" target="_blank">
-                <input name="q" />>
+            <h1>
+                <span>G</span>
+                <span>o</span>
+                <span>o</span>
+                <span>g</span>
+                <span>l</span>
+                <span>e</span>
+            </h1>
+            <form
+                action="http://google.com/search?q=12121"
+                target="_blank"
+                method="GET"
+                onSubmit={e => console.log(e.target.submit())}
+                ref={formRef}
+            >
+                {/* <input name="q" /> */}
+                <input
+                    ref={nomalSearchRef}
+                    name={`q`}
+                    onChange={handleOnChange}
+                    value={searchValue}
+                />
+                {/* https://www.google.com/search?q=your+search+query+here&btnI= */}
+                {/* https://www.google.com/search?q= */}
+                <Button
+                    title="Google Search"
+                    onClick={() => {
+                        formRef.current.dispatchEvent(new Event('submit'))
+                    }}
+                />
                 <input type="submit" />
             </form>
         </Layout>
--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,12 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production'
 const compression = require('compression')
 const express = require('express')
-
+const cors = require('cors')
 module.exports = app => {
     if (isProd) {
         app.use(compression)
     } else {
     }
-
+    app.use(cors())
     app.use(express.json())
 }
